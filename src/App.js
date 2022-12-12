11feee1f46700213022adb5bd944a0699c092897
DIFU-N <chukwudifunsofor@gmail.com>
2022-12-12 11:23:59 +0100

    Analytics Component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Analytics from './components/Analytics';
 import Herro from './components/Herro';
 import NavBar from './components/NavBar';
+import NewsLetter from './components/NewsLetter';
 
 function App() {
   return (
     <div> 
       <NavBar /> 
       <Herro />
-      <Analytics />    
+      <Analytics />
+      <NewsLetter />    
     </div>
   );
 }
